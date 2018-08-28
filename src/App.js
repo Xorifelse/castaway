6b1a332cc6737ed7a67136b90db4c00e087a6ed8
Xorifelse <johan.kruse.com>
2018-08-28 12:31:08 +0200

    Added component to index

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route, Link, Redirect} from 'react-router-dom'
 
 import RegisterWhoContainer from './components/RegisterWhoContainer'; 
 import RegisterLocal from './components/RegisterLocalContainer';
+import RegisterDatePickerContainer from './components/RegisterDatePickerContainer';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route path="/who" component={RegisterWhoContainer} />
 
         <Route path="/chooseLocation" component={RegisterLocal} />
+        <Route path="/selectdate" component={RegisterDatePickerContainer} />
       </div>
     );
   }
