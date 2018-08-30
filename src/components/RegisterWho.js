1b61864988a711134b9070db996327f8e2a03209
Peter van Dijk <petervandijk@gmail.com>
2018-08-30 11:46:30 +0200

    started

--- src/components/RegisterWho.js
+++ src/components/RegisterWho.js
@@ -6,6 +6,7 @@ import Button         from '@material-ui/core/Button'
 import Grid           from '@material-ui/core/Grid'
 import Typography     from '@material-ui/core/Typography'
 import TextField      from '@material-ui/core/TextField'
+import {MDCTopAppBar} from '@material/top-app-bar/index'
 
 // import { allthepeople } from '../lib/People'
 const displayLookingFor = (props) => {
