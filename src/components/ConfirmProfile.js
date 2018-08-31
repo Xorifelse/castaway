0f6d08231095a886014c9e7c3ad345d5130660f2
Xorifelse <johan.kruse.com>
2018-08-31 11:31:51 +0200

    reverted back to original confirm profile

--- src/components/ConfirmProfile.js
+++ src/components/ConfirmProfile.js
@@ -5,8 +5,6 @@ import Grid from '@material-ui/core/Grid'
 import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
 
-import breadcrumbs_state from '../img/breadcrumbs_state04.png'
-
 function getFormattedDate(dateStamp) {
   const date = new Date(dateStamp)
   var year = date.getFullYear();
@@ -24,18 +22,8 @@ function getFormattedDate(dateStamp) {
 
 
 function ConfirmProfile(props) {
-  const { classes } = props;
-
+  
   return (
-    <Grid container spacing={16} direction="column" alignItems="left" className={classes.content}>
-      <Grid item className={classes.topBarBreadcrumbs}>
-        <Grid item className={classes.topBarBreadcrumbsPNG}><img src={breadcrumbs_state} width="80px" /></Grid>
-      </Grid>
-    </Grid>
-  )
-}
-
-/*
     <Grid container spacing={16} direction="column" justify="center" alignItems="center">
       <Grid item>
         <Typography variant="headline" component="h2">This is you. Happy?</Typography>
@@ -78,107 +66,27 @@ function ConfirmProfile(props) {
       <Grid item>
         <Grid container spacing={16} direction="row" justify="center" alignItems="center">
           <Grid item><Button variant='outlined' color="primary" onClick={() => props.dbAddFn(props.userObj)}>SUBSCRIBE</Button></Grid>
-          {/*  ==== SUBSCRIBE BUTTON ADDED FOR TESTING  *\/}
+          {/*  ==== SUBSCRIBE BUTTON ADDED FOR TESTING  */}
         </Grid>
       </Grid>
       <Grid item>
         <Grid container spacing={16} direction="row" justify="center" alignItems="center">
           <Grid item><Button variant='outlined' color="primary" ><Link to="/search">FEED ME!</Link></Button></Grid>
-          {/* disabled={(props.userObj.avatar) ? false : true} ====== REMOVED FOR TESTING - ADD TO BUTTON FEED ME *\/} 
-          {/* onClick={() => props.dbAddFn(props.userObj)}  ==== REMOVED FOR TESTING  - ADD TO BUTTON FEED ME *\/}
+          {/* disabled={(props.userObj.avatar) ? false : true} ====== REMOVED FOR TESTING - ADD TO BUTTON FEED ME */} 
+          {/* onClick={() => props.dbAddFn(props.userObj)}  ==== REMOVED FOR TESTING  - ADD TO BUTTON FEED ME */}
         </Grid>
-*/
+      </Grid>
 
-const styles = theme => ({
-  topBar: {
-    background: '#BE8D8A',
-    textAlign: "center",
-    width: '100%',
-    height: '70px',
-  },
-  topBarBreadcrumbs: {
-    background: '#EBF0FF',
-    textAlign: "center",
-    width: '100%',
-    height: '30px'
-  },
-  topBarBreadcrumbsPNG: {
-    marginTop: 2,
-  },
-  txtSmall: {
-    color: 'white',
-  },
-  txtBig: {
-    color: 'white',
-  },
-  textField: {
-    width: 250
-  },
-  welcome: {
-    background: '#a1a1a1',
-    textAlign: "center"
-  },
-  hi: {
-    background: '#a1a1a1',
-    padding: '15px 0 15px 0',
-  },
-  who: {
-    fontSize: '30px',
-    background: '#a1a1a1',
-    padding: '0 0 10px 0',
-    textAlign: "center"
-  },
-  content: {
- 
-    background: '#EBF0FF',
-    height: '100vh',
-  },
-  name: {
-    marginTop: 40,
-    marginLeft: 40
-  },
-  type: {
-    marginTop: 20,
-    marginLeft: 40
-  },
-  card: {
-    minWidth: 150
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    marginBottom: 16,
-    fontSize: 18,
-  },
-  pos: {
-    // marginBottom: 24,
-  },
-  header: {
-    marginTop: 16,
-    marginBottom: 16,
-    fontSize: 48
-  },
-  colorSwitchBase: {
-    color: 'primary',
-    '&$colorChecked': {
-      color: 'primary',
-      '& + $colorBar': {
-        backgroundColor: 'primary',
-      },
-    },
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
-  },
-});
+          
+          
+          {/* 
+           */}
+    </Grid>
+  )
+}
+
+const styles ={
+
+};
 
 export default withStyles(styles)(ConfirmProfile)
